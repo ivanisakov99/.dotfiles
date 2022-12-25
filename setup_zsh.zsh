abcd2a0a964027338ee782d464e4cd973d5c31bf
Ivan Isakov <60196280+ivanisakov99@users.noreply.github.com>
2022-12-25 16:21:21 -0500

    Prevent superfluous commands with conditionals

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -2,10 +2,23 @@
 
 echo "\n<<< Starting ZSH Setup >>>\n"
 
-echo "Enter superuser (sudo) password to edit /ets/shells"
-echo '/opt/homebrew/bin/zsh' | sudo tee -a 'etc/shells' >/dev/null
+local new_shell='/opt/homebrew/bin/zsh'
 
+# https://stackoverflow.com/a/4749368/1341838
+if grep -Fxq $new_shell '/etc/shells'; then
+    echo "$new_shell already exists in /etc/shells"
+else
+echo "Enter superuser (sudo) password to edit /ets/shells"
+    echo $new_shell | sudo tee -a '/etc/shells' >/dev/null
+fi
+echo "\n"
+
+if [ "$SHELL" = "$new_shell" ]; then
+    echo "SHELL is already $new_shell"
+else
 echo "Enter user password to change login shell"
-chsh -s '/opt/homebrew/bin/zsh'
+    chsh -s $new_shell
+fi
+echo "\n"
 
 echo "\n<<< ZSH Setup Complete >>>\n"
