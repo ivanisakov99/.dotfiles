73243cd1667757deaab7e1358fff6a1436d440b3
Ivan Isakov <60196280+ivanisakov99@users.noreply.github.com>
2022-12-25 16:21:50 -0500

    Symlink `sh` to `zsh`

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -8,7 +8,7 @@ local new_shell='/opt/homebrew/bin/zsh'
 if grep -Fxq $new_shell '/etc/shells'; then
     echo "$new_shell already exists in /etc/shells"
 else
-echo "Enter superuser (sudo) password to edit /ets/shells"
+    echo "Enter superuser (sudo) password to edit /ets/shells"
     echo $new_shell | sudo tee -a '/etc/shells' >/dev/null
 fi
 echo "\n"
@@ -16,8 +16,20 @@ echo "\n"
 if [ "$SHELL" = "$new_shell" ]; then
     echo "SHELL is already $new_shell"
 else
-echo "Enter user password to change login shell"
+    echo "Enter user password to change login shell"
     chsh -s $new_shell
+fi
+echo "\n"
+
+if sh --version | grep -q zsh; then
+    echo "/prvate/var/select/sh already linked to /bin/zsh"
+else
+    echo "Enter superuser (sudo) password to symlink sh to zsh"
+    # Looked cute, might delete later, idk
+    sudo ln -sfv /bin/zsh /private/var/select/sh
+
+    # TODO I'd like for this to work instead.
+    # sudo ln -svf /opt/homebrew/bin/zsh /private/var/select/sh
 fi
 echo "\n"
 
