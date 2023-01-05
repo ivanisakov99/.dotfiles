1bca4da6587adc6813b083c0596774a725c628a4
Ivan Isakov <60196280+ivanisakov99@users.noreply.github.com>
2023-01-04 23:36:36 -0500

    Make Finder show the path bar

---
+++ setup_macos.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting MacOS Setup >>>\n"
+
+###############################################################################
+# Finder                                                                      #
+###############################################################################
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+echo "\n<<< MacOS Setup Complete >>>\n"
